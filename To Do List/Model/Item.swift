f7b7f087e63b37beb3f10b0e5b61e129727a40d6
nate <nathanael.j.howell@biola.edu>
2021-07-19 15:55:40 -0700

    Add Model

---
+++ To Do List/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  To Do List
+//
+//  Created by Nate Howell on 7/19/21.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    @objc dynamic var title: String = ""
+    @objc dynamic var done: Bool = false
+}
