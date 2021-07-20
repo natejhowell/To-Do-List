366de3555524fd56ff37911949da4cdc8dc13859
nate <nathanael.j.howell@biola.edu>
2021-07-19 18:28:37 -0700

    update tableview datasource methods

--- To Do List/Controller/ViewController.swift
+++ To Do List/Controller/ViewController.swift
@@ -10,7 +10,7 @@ import RealmSwift
 
 class ViewController: UITableViewController {
 
-    var toDoItems: Results<Item>?
+    var toDoItems: [Item] = []
     let realm = try! Realm()
 
     override func viewDidLoad() {
@@ -21,20 +21,21 @@ class ViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return toDoItems?.count ?? 1
+        return toDoItems.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { return UITableViewCell() }
 
-        if let item = toDoItems?[indexPath.row]  {
-
-            cell.textLabel?.text = item.title
-            cell.accessoryType = item.done ? .checkmark : .none
-        } else {
-            cell.textLabel?.text = "No Items Added"
-        }
+//        if let item = toDoItems[indexPath.row] {
+//
+//        } else {
+//            cell.textLabel?.text = "No Items Added"
+//        }
+        let item = toDoItems[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
 
         return cell
     }
