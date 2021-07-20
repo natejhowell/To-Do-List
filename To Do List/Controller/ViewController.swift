ec051d5eb1494538ea426d59d027a26a80dd64c9
Shayne Torres <shayneptorres@gmail.com>
2021-07-19 18:36:28 -0700

    add todo comments for nate

--- To Do List/Controller/ViewController.swift
+++ To Do List/Controller/ViewController.swift
@@ -9,57 +9,63 @@ import UIKit
 import RealmSwift
 
 class ViewController: UITableViewController {
-
+    
     var toDoItems: [Item] = []
     let realm = try! Realm()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // TODO: call the refreshData() method here
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-
+    
+    func refreshData() {
+        // TODO: This is the method where you will update "toDoItems" with what currently in your database. Look up how to fetch realm data and do that here. This will require learning how to convert Results/List into an Array
+    }
+    
+    func saveItem(text: String) {
+        // TODO: This is the method where you will add an item to the Realm database with the given text. Make sure you call this where you have access to the textfields value
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return toDoItems.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { return UITableViewCell() }
-
-//        if let item = toDoItems[indexPath.row] {
-//
-//        } else {
-//            cell.textLabel?.text = "No Items Added"
-//        }
+        
         let item = toDoItems[indexPath.row]
         cell.textLabel?.text = item.title
         cell.accessoryType = item.done ? .checkmark : .none
-
+        
         return cell
     }
-
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
     
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
         var textField = UITextField()
-
-    let alert = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
-
-    let action = UIAlertAction(title: "Add", style: .default) { (action) in
-    }
-
+        
+        let alert = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            // TODO: Make the call to save the item to the database with the textfields value
+            // TODO: Make sure to call the refreshData() method to keep your table view up to date with you Realm Database
+        }
+        
         alert.addTextField { (alertTextField) in
-        alertTextField.placeholder = "Create new item"
-        textField = alertTextField
-
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+            
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
-
-    alert.addAction(action)
-
-    present(alert, animated: true, completion: nil)
-
-}
-
+    
 }
 
