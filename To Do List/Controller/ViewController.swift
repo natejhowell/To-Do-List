5115607be5e8048eab6cd613e9349db7dabd3e68
nate <nathanael.j.howell@biola.edu>
2021-07-20 17:20:32 -0700

    Implement save and cheakmark feature

--- To Do List/Controller/ViewController.swift
+++ To Do List/Controller/ViewController.swift
@@ -10,38 +10,70 @@ import RealmSwift
 
 class ViewController: UITableViewController {
     
-    var toDoItems: [Item] = []
+    var toDoItems: Results<Item>?
     let realm = try! Realm()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // TODO: call the refreshData() method here
+        refreshData()
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
     func refreshData() {
-        // TODO: This is the method where you will update "toDoItems" with what currently in your database. Look up how to fetch realm data and do that here. This will require learning how to convert Results/List into an Array
+        
+        toDoItems = realm.objects(Item.self)
+        
     }
     
-    func saveItem(text: String) {
-        // TODO: This is the method where you will add an item to the Realm database with the given text. Make sure you call this where you have access to the textfields value
+    func saveItem(item: Item) {
+        
+            do {
+                try realm.write {
+                    realm.add(item)
+                }
+            } catch {
+                print("Error saving item \(error)")
+            }
+            
+            tableView.reloadData()
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return toDoItems.count
+        return toDoItems?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { return UITableViewCell() }
         
-        let item = toDoItems[indexPath.row]
+        if let item = toDoItems?[indexPath.row] {
         cell.textLabel?.text = item.title
         cell.accessoryType = item.done ? .checkmark : .none
+        } else {
+            cell.textLabel?.text = "No Items added"
+        }
         
         return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let item = toDoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+                }
+            } catch {
+                print("Error saving done status, \(error)")
+            }
+        }
+        
+        tableView.reloadData()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -51,18 +83,24 @@ class ViewController: UITableViewController {
         let alert = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            // TODO: Make the call to save the item to the database with the textfields value
-            // TODO: Make sure to call the refreshData() method to keep your table view up to date with you Realm Database
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.refreshData()
+            
+            self.saveItem(item: newItem)
         }
         
-        alert.addTextField { (alertTextField) in
-            alertTextField.placeholder = "Create new item"
-            textField = alertTextField
-            
-        }
         
         alert.addAction(action)
-        
+        alert.addTextField { (alertTextField) in
+            textField = alertTextField
+            alertTextField.placeholder = "Create new item"
+            
+            
+        }
+    
         present(alert, animated: true, completion: nil)
         
     }
