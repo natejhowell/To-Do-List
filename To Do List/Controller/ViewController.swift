0f87605d9171ef7e24d5ce151fb75df51407d838
Shayne Torres <shayneptorres@gmail.com>
2021-07-27 18:07:13 -0700

    implement sorting items by date and done

--- To Do List/Controller/ViewController.swift
+++ To Do List/Controller/ViewController.swift
@@ -10,7 +10,7 @@ import RealmSwift
 
 class ViewController: UITableViewController {
     
-    var toDoItems: Results<Item>?
+    var toDoItems: [Item] = []
     let realm = try! Realm()
     
     override func viewDidLoad() {
@@ -23,14 +23,12 @@ class ViewController: UITableViewController {
     }
     
     func refreshData() {
-        
-        toDoItems = realm.objects(Item.self)
-        toDoItems = toDoItems?.sorted(byKeyPath: "dateCreated", ascending: false)
-        
+        toDoItems = Array(realm.objects(Item.self))
+        toDoItems.sort(by: { $0.dateCreated > $1.dateCreated }) // sort by dateCreated, newer items showing first
+        toDoItems.sort(by: { item, _ in !item.done }) // sort by done, with not done items showing first
     }
     
     func saveItem(item: Item) {
-        
             do {
                 try realm.write {
                     realm.add(item)
@@ -40,24 +38,25 @@ class ViewController: UITableViewController {
             }
         
             tableView.reloadData()
-        
     }
     
     // MARK: - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return toDoItems?.count ?? 1
+        return toDoItems.isEmpty ? 1 : toDoItems.count // if toDoItems is empty, show 1 cell with the "No items added" text
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") else { return UITableViewCell() }
         
-        if let item = toDoItems?[indexPath.row] {
-        cell.textLabel?.text = item.title
-        cell.accessoryType = item.done ? .checkmark : .none
+        if toDoItems.isEmpty {
+            cell.textLabel?.text = "No Items added"  // make sure there are items in the toDoItems array before selecting
         } else {
-            cell.textLabel?.text = "No Items added"
+            let item = toDoItems[indexPath.row]
+            
+            cell.textLabel?.text = item.title
+            cell.accessoryType = item.done ? .checkmark : .none
         }
         
         return cell
@@ -67,20 +66,20 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        if let item = toDoItems?[indexPath.row] {
-            do {
-                try realm.write {
-                    item.done = !item.done
-                }
-            } catch {
-                print("Error saving done status, \(error)")
+        guard !toDoItems.isEmpty else { return } // make sure there are items in the toDoItems array before selecting
+        
+        let item = toDoItems[indexPath.row]
+        do {
+            try realm.write {
+                item.done = !item.done
             }
+        } catch {
+            print("Error saving done status, \(error)")
         }
         
+        refreshData()
         tableView.reloadData()
-        
         tableView.deselectRow(at: indexPath, animated: true)
-        
     }
     
     // MARK: - Add New Items
@@ -95,11 +94,10 @@ class ViewController: UITableViewController {
             
             let newItem = Item()
             newItem.title = textField.text!
-            
-            self.refreshData()
-            
+                        
             self.saveItem(item: newItem)
-            
+            self.refreshData()
+            self.tableView.reloadData()
         }
         
         alert.addAction(action)
